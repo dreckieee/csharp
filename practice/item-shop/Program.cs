c2d406b2ef89d1d37c01c65c8ce8bbba0ac165dd
dreckieee <dreckrjpascual@gmail.com>
2026-05-23 22:41:11 +0800

    Added unfinished item-shop project

---
+++ practice/item-shop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\nHello, ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Dreckieee");
+        Console.ResetColor();
+        Console.WriteLine("!");
+
+        
+        
+    }//end of Main method
+
+
+    public static string ReadString(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Cannot be empty. Try again.");
+            }
+            else
+            {
+                return input;
+            }
+        }
+    }//end of ReadString method
+
+
+}//end of Program method
