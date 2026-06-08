ba1ebda507ae344ba55eeac6cef4a317fa5c5e27
dreckieee <dreckrjpascual@gmail.com>
2026-06-08 21:36:10 +0800

    added initial bank-system console app

---
+++ practice/bank-system/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("\nWelcome, dreckieee!\n");
+        
+    }//end of Main method
+}//end of Program class
