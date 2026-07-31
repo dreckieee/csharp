ff33735f1089764f2a4433a91a40e1749e9daba5
dreckieee <dreckrjpascual@gmail.com>
2026-07-31 08:23:08 +0800

    refactor: remove template comments and extra whitespace

--- practice/IncomeTracker/Program.cs
+++ practice/IncomeTracker/Program.cs
@@ -4,18 +4,15 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<IncomeTrackerDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("IncomeTracker")));
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
